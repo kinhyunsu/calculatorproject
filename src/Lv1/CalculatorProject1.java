3188f35b681ce69532551e825f120670f81d0cdc
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-04-16 23:37:43 +0900

    ã완료 결과물

--- src/Lv1/CalculatorProject1.java
+++ src/Lv1/CalculatorProject1.java
@@ -5,50 +5,51 @@ public class CalculatorProject1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("첫 번째 정수를 입력해주세요 ! :");
-        int num1 = scanner.nextInt();
 
-        System.out.println("연산자를 입력해주세요 ! : (+,-,*,/)");
-        char operator = scanner.next().charAt(0);
-
-
-        System.out.println("두 번째 정수를 입력해주세요 ! :");
-        int num2 = scanner.nextInt();
-
-        int result = 0;
-
-        switch (operator){
-            case '+' :
-                result = num1 + num2;
-                break;
-            case '-' :
-                result = num1 - num2;
-                break;
-            case '*' :
-                result = num1 * num2;
-                break;
-            case '/' :
-                if (num2 !=0){
-                    result = num1 / num2;
-                } else {
-                    System.out.println("0으로 나눌 수 없습니다!");
-                }
-
-            default:
-
-                System.out.println("올바른 연산자가 아닙니다 ! ");
-                return;
-        }
-        System.out.println("결과입니다 ! : " + result);
-        while(true){
-            System.out.println("exit를 누르면 종료됩니다.");
+        while(true) {
+            System.out.println("exit를 누르면 종료되며 아무키나 입력하시면 실행됩니다.");
             String input = scanner.next();
-            if (input.equals("exit")){
+            if (input.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
-        }
 
+            System.out.println("첫 번째 정수를 입력해주세요 ! :");
+            int num1 = scanner.nextInt();
+
+            System.out.println("연산자를 입력해주세요 ! : (+,-,*,/)");
+            char operator = scanner.next().charAt(0);
+
+
+            System.out.println("두 번째 정수를 입력해주세요 ! :");
+            int num2 = scanner.nextInt();
+
+            int result = 0;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("0으로 나눌 수 없습니다!");
+                    }
+
+                default:
+
+                    System.out.println("올바른 연산자가 아닙니다 ! ");
+                    return;
+            }
+            System.out.println("결과입니다 ! : " + result);
+        }
 
     }
 }
