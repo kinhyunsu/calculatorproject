3f6852b46812df668e4efbeb526802c60737cb00
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-04-17 17:33:13 +0900

    리스트 보기 기능 추가 및 배치 수정

--- src/Lv2/Main.java
+++ src/Lv2/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
-        System.out.println("명령어를 입력해주세요 !(go, exit) :");
+        System.out.println("명령어를 입력해주세요 (go, exit, delete, list) :");
         while(true){
             try{
                 String input = scanner.next();
@@ -17,34 +17,43 @@ public class Main {
                 }else if (input.equals("exit")){
                     System.out.println("시스템을 종료합니다.");
                     break;
-                }else{
+                }else if (input.equals("list")){
+                    System.out.println("현재 저장된 결과 리스트");
+                    System.out.println(calculator.getResultList());
+                }else if (input.equals(("delete"))) {
+                Integer removed = calculator.removeResult();
+                if (removed != null) {
+                    System.out.println("가장 오래된 결과 " + removed + "가 삭제되었습니다.");
+                }
+                    continue;
+                }else {
                     System.out.println("알 수 없는 명령어입니다.");
                     break;
                 }
-                System.out.println("첫 번째 수를 입력해주십쇼 ! :");
+                System.out.println("첫 번째 수를 입력해주십쇼  :");
                 int num1 = scanner.nextInt();
                 if(num1 < 0){
-                    System.out.println("음수는 입력할 수 없습니다!");
+                    System.out.println("음수는 입력할 수 없습니다");
                     continue;
                 }
-                System.out.println("사칙연산 기호를 입력해주십쇼 ! :");
+                System.out.println("사칙연산 기호를 입력해주십쇼 :");
                 char operator = scanner.next().charAt(0);
 
-                System.out.println("두 번째 수를 입력해주십쇼! : ");
+                System.out.println("두 번째 수를 입력해주십쇼 : ");
                 int num2 = scanner.nextInt();
                 if(num2 < 0){
-                    System.out.println("두 번째 수도 음수 입력은 불가합니다 ! ");
+                    System.out.println("두 번째 수도 음수 입력은 불가합니다 ");
                     continue;
                 }
                 int result = calculator.calculator(num1, num2, operator);
                 System.out.println("결과입니다 ! : " + result);
             }catch (InputMismatchException e){
-                System.out.println("숫자만 입력가능합니다!");
+                System.out.println("숫자만 입력가능합니다");
                 scanner.nextLine();
             }catch (ArithmeticException e){
                 System.out.println("계산 중 오류 발생 : " + e.getMessage());
             }
-            System.out.println("명령어를 입력해주세요 !(go, exit) :");
+            System.out.println("명령어를 입력해주세요 (go, exit, list, delete) :");
         }
 
     }
